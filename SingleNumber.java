0233e732fbe974e2195ea7a9e7a386e6dd4ed796
Yudhajeet Bhattacharya <57074118+norserambler@users.noreply.github.com>
2025-02-06 22:30:31 +0530

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,20 @@
+// Question Number 136 - https://leetcode.com/problems/single-number/description/
+
+class Solution {
+
+/* Need to use the properties of xor
+    n^n =0 and 0 ^n =n
+    xor every number with one another in a sequential manner
+    ex array - [2,2,1]
+    Result = 2^2^1 = 0^1 = 0
+*/
+
+    public int singleNumber(int[] nums) {
+        int result =0;
+        for(int num:nums)
+            result = result ^ num;
+
+        return result;
+    }
+
+}
